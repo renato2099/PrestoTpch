20e03f6941e3326d6ac47d711a28baad560cba23
Renato Marroquin <renatoj.marroquin@gmail.com>
2016-02-16 22:48:25 +0100

    Fixing queries

--- sql/tpch_query22.sql
+++ sql/tpch_query22.sql
@@ -3,44 +3,17 @@ select
 	count(1) as numcust,
 	sum(c_acctbal) as totacctbal
 from (
-	select
-		cntrycode,
-		c_acctbal,
-		avg_acctbal
-	from
-		(select
-			avg(c_acctbal) as avg_acctbal
-		from
-			(select
-					c_acctbal,
-					c_custkey,
-					substr(c_phone, 1, 2) as cntrycode
-				from
-					customer c
-				where
-					substr(c_phone, 1, 2) IN ('13', '31', '23', '29', '30', '18', '17'))
-		) ct1 
-		cross join (
-			select
-				cntrycode,
-				c_acctbal
-			from
-				(select o_custkey from orders o group by o_custkey) ot
-			right outer join (select
-									c_acctbal,
-									c_custkey,
-									substr(c_phone, 1, 2) as cntrycode
-								from
-									customer c
-								where
-									substr(c_phone, 1, 2) IN ('13', '31', '23', '29', '30', '18', '17')) ct
+	select cntrycode, c_acctbal, avg_acctbal from
+	(select avg(c_acctbal)as avg_acctbal from customer c where c_acctbal > 0.00 and substr(c_phone, 1, 2) IN ('13', '31', '23', '29', '30', '18', '17')) as ct1
+	cross join (
+			select cntrycode, c_acctbal from (select o_custkey from orders o group by o_custkey) ot
+			right outer join (select c_acctbal, c_custkey, substr(c_phone, 1, 2) as cntrycode from customer c where substr(c_phone, 1, 2) IN ('13', '31', '23', '29', '30', '18', '17')) ct
 			on ct.c_custkey = ot.o_custkey
 			where ot.o_custkey is null
-		) ct2
-) a
-where
-	c_acctbal > avg_acctbal
+	) as ct2
+	where c_acctbal > avg_acctbal
+)a
 group by
 	cntrycode
 order by
-	cntrycode
+	cntrycode;
--- sql/tpch_query4.sql
+++ sql/tpch_query4.sql
@@ -2,13 +2,20 @@ select
 	o_orderpriority,
 	count(*) as order_count
 from
-	orders as o,
-    lineitem l
+(
+select 	o_orderpriority, o_orderkey
+
+from	orders as o,
+        lineitem l
 where
 	o_orderdate >= date '1993-07-01'
 	and o_orderdate <  date '1993-07-01' + interval '3' month
 	and l_orderkey = o_orderkey
 	and l_commitdate < l_receiptdate
+group by
+	o_orderpriority, o_orderkey
+) as tmp
+
 group by
 	o_orderpriority
 order by
